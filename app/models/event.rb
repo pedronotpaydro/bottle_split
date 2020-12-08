5a50fcf5843991d31a41a5b0feb281171b2a6759
Pedro <orsomethingelse@gmail.com>
2020-12-08 13:01:57 -0600

    started validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  validates :name, presence: true, length: { in: 5..50 }
+  validates :location, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,86 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# beer = Beer.create(
+#   name: "Benthic",
+#   brewery: "Half Acre Beer Company",
+#   style: "Stout",
+#   description: "Bourbon Barrel Aged Imperial Stout brewed with coffee and toasted coconut. A remedy coaxed from the deepest caverns; it's char bound in the systematic sweetness and density only found in the colossal beers of our time.",
+# )
+# beer = Beer.create(
+#   name: "Bourbon County Brand Stout",
+#   brewery: "Goose Island Beer Co",
+#   style: "Stout",
+#   description: "Originally brewed in honor of the 1000th batch at our original Clybourn brewpub. A liquid as dark and dense as a black hole with thick foam the color of a bourbon barrel. The nose is an intense mix of charred oak, chocolate, vanilla, caramel and smoke. One sip has more flavor than your average case of beer",
+# )
+# beer = Beer.create(
+#   name: "Kentucky Breakfast Stout (KBS)",
+#   brewery: "Founders Brewing Co",
+#   style: "Stout",
+#   description: "This beer taught us that patience truly is a virtue. KBS is a big imperial stout brewed with a massive amount of coffee and chocolate and then bourbon barrel-aged to perfection. Incredibly silky and full-bodied with notes of vanilla, cocoa, roasted coffee and charred oak",
+# )
+# beer = Beer.create(
+#   name: "Dark Lord",
+#   brewery: "3 Floyds Brewing Co",
+#   style: "Stout",
+#   description: "A demonic Russian-Style Imperial Stout brewed with coffee, Mexican vanilla, and Indian sugar, this beer defies description:. Available one day a year, in April at the brewery: Dark Lord Day.",
+# )
+# beer = Beer.create(
+#   name: "Barrel-Aged Abraxas",
+#   brewery: "Perennial Artisan Ales",
+#   style: "Stout",
+#   description: "Imperial Stout aged Twelve months in Rittenhouse Rye barrels with cacao nibs, vanilla beans, ancho chiles & cinnamon sticks",
+# )
+# beer = Beer.create(
+#   name: "Dino S'mores",
+#   brewery: "Off Color Brewing",
+#   style: "Stout",
+#   description: "Imperial stout brewed w/ graham flour, marshmallow, molasses, cocoa nibs & vanilla bean",
+# )
+# beer = Beer.create(
+#   name: "Barrel-Aged Henna: Karma",
+#   brewery: "More Brewing Company",
+#   style: "Stout",
+#   description: "Imperial Stout aged in a mix of Buffalo Trace and Heaven Hill barrels for 10 months then aged on Cocoa Nibs, Cinnamon & Toasted Coconut. (2018 bottle batch)",
+# )
+# beer = Beer.create(
+#   name: "Cuppa Brûlée",
+#   brewery: "Maplewood Brewing Company",
+#   style: "Stout",
+#   description: "Our insanely thick and robust stout is brewed with burnt panella sugar, a blend of Madagascar and Papua New Guinea vanilla beans and a hint of cinnamon. Those flavors are complemented with subtle coffee notes from a blend of beans hand picked with Chicago’s Metric Coffee.",
+# )
+# beer = Beer.create(
+#   name: "Catch Hell",
+#   brewery: "Half Acre Beer Company",
+#   style: "Stout",
+#   description: "A gravity rich submission aged in Buffalo Trace Bourbon barrels and brewed with Madagascar Vanilla and Cinnamon. We sought balance across each element of the recipe.
+
+#   All told, this beer lands at 14%, but the experience drinking it tucks all of the ethanol away in place of a luscious, two-ton mellow.
+
+#   Vanilla, BBL driven coconut, bourbon, cinnamon, cherry, chocolate and density all equally synthesized. A really deep beer that lands full panorama",
+# )
+# beer = Beer.create(
+#   name: "Barrel Aged Imperial Pajamas",
+#   brewery: "Begyle Brewing",
+#   style: "Stout",
+#   description: "Imperial Pajamas is our imperial oatmeal stout aged in Heaven Hill Bourbon barrels.",
+# )
+
+# event = Event.create(
+#   name: "Test Event One",
+#   location: "Parts Unknown",
+#   date: "2021/01/29 17:00",
+
+# )
+# event = Event.create(
+#   name: "Test Event Two",
+#   location: "Parts Unknown",
+#   date: "2021/01/22 17:00",
+
+# )
+# event = Event.create(
+#   name: "Test Event Three",
+#   location: "Parts Unknown",
+#   date: "2021/01/15 17:00",
+
+# )
